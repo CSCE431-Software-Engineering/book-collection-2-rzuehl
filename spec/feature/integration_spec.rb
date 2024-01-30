b0081ed7308a102c175c9786d68cb7e4bafd9cc5
Ryan Zuehl <rzuehl@tamu.edu>
2024-01-29 21:21:54 -0600

    Added tests for new attributes

--- spec/feature/integration_spec.rb
+++ spec/feature/integration_spec.rb
@@ -5,13 +5,53 @@ RSpec.describe 'Creating a book', type: :feature do
   scenario 'valid inputs' do
     visit new_book_path
     fill_in "book[title]", with: 'harry potter'
+    fill_in "book[author]", with: 'Ryan'
+    fill_in "book[price]", with: 10.00
+    fill_in "book[date]", with: '2022-10-01'
     click_on 'Create Book'
     expect(page).to have_content('Book was successfully created')
   end
 
+  #test invalid title
   scenario 'invalid title' do
     visit new_book_path
-    fill_in "book[title]", with: ""
+    fill_in "book[title]", with: nil
+    fill_in "book[author]", with: 'Ryan'
+    fill_in "book[price]", with: 10.00
+    fill_in "book[date]", with: '2022-10-01'
+    click_on 'Create Book'
+    expect(page).to_not have_content('Book was successfully created')
+  end
+
+  #test invalid author
+  scenario 'invalid author' do
+    visit new_book_path
+    fill_in "book[title]", with: 'harry potter'
+    fill_in "book[author]", with: nil
+    fill_in "book[price]", with: 10.00
+    fill_in "book[date]", with: '2022-10-01'
+
+    click_on 'Create Book'
+    expect(page).to_not have_content('Book was successfully created')
+  end
+
+  #test invalid price
+  scenario 'invalid price' do
+    visit new_book_path
+    fill_in "book[title]", with: 'harry potter'
+    fill_in "book[author]", with: 'Ryan'
+    fill_in "book[price]", with: nil
+    fill_in "book[date]", with: '2022-10-01'
+    click_on 'Create Book'
+    expect(page).to_not have_content('Book was successfully created')
+  end
+
+  scenario 'invalid date' do
+    visit new_book_path
+    fill_in "book[title]", with: 'harry potter'
+    fill_in "book[author]", with: 'Ryan'
+    fill_in "book[price]", with: 10.00
+    fill_in "book[date]", with: nil
     click_on 'Create Book'
     expect(page).to_not have_content('Book was successfully created')
   end
--- spec/unit/unit_spec.rb
+++ spec/unit/unit_spec.rb
@@ -2,16 +2,33 @@
 require 'rails_helper'
 
 RSpec.describe Book, type: :model do
+  #create the subject
   subject do
-    described_class.new(title: 'harry potter')
+    described_class.new(title: 'harry potter', author: 'Ryan', price: 10.00, date: "2022-10-01")
   end
 
+  #test valid inputs
   it 'is valid with valid attributes' do
     expect(subject).to be_valid
   end
 
   it 'is not valid without a name' do
     subject.title = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without an author' do
+    subject.author = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a price' do
+    subject.price = nil
+    expect(subject).not_to be_valid
+  end
+
+  it 'is not valid without a date' do
+    subject.date = nil
     expect(subject).not_to be_valid
   end
 end
